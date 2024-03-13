39d126c4ec20489e349d8728d731f23b43347f5d
Miguel Brown C <browncoloma@gmail.com>
2024-03-13 17:11:23 -0300

    data creada de guitarras y enlasada a App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <script setup>
+import {ref, reactive} from 'vue'
+import {db} from './data/guitarras'
 
+const state = reactive({
+  guitarras: db
+})
+
+console.log(state.guitarras);
 </script>
 
 <template>
